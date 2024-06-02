514db94cf1f101ddafa62833c3c2a7de2d32dff3
AF Tamim <121453153+AFTamim123@users.noreply.github.com>
2024-06-02 23:40:57 +0600

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,63 +1,28 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-class DSU {
-    vector<int> size,parent,rank;
-public:
-    DSU(int n) {
-        rank.resize(n+1, 0);
-        parent.resize(n+1);
-        size.resize(n+1);
-        for (int i = 0; i <= n; i++)
-        {
-            parent[i]=i;
-            size[i]=1;
-        }
-    }
-    int finduparent(int node) {
-        if(node==parent[node]) 
-            return node;
-        return parent[node] = finduparent(parent[node]);
-    }
-    void unionbyrank(int u, int v) {
-        int u_parent = finduparent(u);
-        int v_parent = finduparent(v);
-        if(u_parent==v_parent) return;
-        if(rank[v_parent]>rank[u_parent]) {
-            parent[u_parent]=v_parent;
-        }
-        else if(rank[v_parent]<rank[u_parent]) {
-            parent[v_parent]=u_parent;
-        }
-        else {
-            parent[v_parent]=parent[u_parent];
-            rank[u_parent]++;
-        }
-    }
-    void unionbysize(int u, int v) {
-        int u_parent = finduparent(u);
-        int v_parent = finduparent(v);
-        if(u_parent==v_parent) return;
-        if(size[v_parent]>size[u_parent]) {
-            parent[u_parent]=v_parent;
-            size[v_parent]+=size[u_parent];
-        }
-        else {
-            parent[v_parent]=u_parent;
-            size[u_parent]+=size[v_parent];
-        }
-    }
- 
-    int getSize(int node) {
-        return size[finduparent(node)];
-    }
+struct DSU {
+  vector<int> par, rnk, sz;
+  int c;
+  DSU(int n) : par(n + 1), rnk(n + 1, 0), sz(n + 1, 1), c(n) {
+    for (int i = 0; i <= n; ++i) par[i] = i;
+  }
+  int find(int i) {
+    return (par[i] == i ? i : (par[i] = find(par[i])));
+  }
+  bool same(int i, int j) {
+    return find(i) == find(j);
+  }
+  int get_size(int i) {
+    return sz[find(i)];
+  }
+  int count() {
+    return c;    //connected components
+  }
+  int merge(int i, int j) {
+    if ((i = find(i)) == (j = find(j))) return -1;
+    else --c;
+    if (rnk[i] > rnk[j]) swap(i, j);
+    par[i] = j;
+    sz[j] += sz[i];
+    if (rnk[i] == rnk[j]) rnk[j]++;
+    return j;
+  }
 };
-
-
-int32_t main() {
-    
-    
-   
-
-    return 0;
-}
